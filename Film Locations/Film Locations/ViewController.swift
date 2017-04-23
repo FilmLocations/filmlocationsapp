64721144755728c032f40a2303603cbbbc64d9ce
Jessica Thrasher <jthrashe@cisco.com>
2017-04-23 15:25:20 -0700

    Adding new project

---
+++ Film Locations/Film Locations/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Film Locations
+//
+//  Created by Jessica Thrasher on 4/23/17.
+//  Copyright © 2017 Codepath Spring17. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
