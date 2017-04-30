534818d5d335c56818baf8463b705f1b6bafccad
LauraPrichici <p_laura_c@yahoo.com>
2017-04-30 00:35:49 -0700

    The FirebaseModel was addapted to parse the InputMovieData json file

--- Film Locations/Film Locations/FirebaseMovie.swift
+++ Film Locations/Film Locations/FirebaseMovie.swift
@@ -7,22 +7,44 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 class FirebaseMovie {
-    var title: String
-    var releaseYear: String
-    var posterImageURL: URL
-    var locationImageURL: URL
-    var address: String
-    var distance: Float
     
-    init(title: String, releaseYear: String, posterImageURL: URL, locationImageURL: URL, address: String, distance: Float)
-    {
-        self.title = title
-        self.releaseYear = releaseYear
-        self.posterImageURL = posterImageURL
-        self.locationImageURL = locationImageURL
-        self.address = address
-        self.distance = distance
+    private let posterStringBaseURL = "http://image.tmdb.org/t/p/w185/"
+    
+    var title: String {
+        return json["title"].stringValue
     }
+    
+    var releaseYear: String {
+        return json["release"]["year"].stringValue
+    }
+    var posterImageURL: URL? {
+        let stringURL = posterStringBaseURL + json["images"]["poster"].stringValue
+        return URL(string: stringURL)
+    }
+    
+    var address: String {
+        return json["location"]["address"].stringValue
+    }
+    
+//    init(title: String, releaseYear: String, posterImageURL: URL, locationImageURL: URL, address: String, distance: Float)
+//    {
+//        self.title = title
+//        self.releaseYear = releaseYear
+//        self.posterImageURL = posterImageURL
+//        self.locationImageURL = locationImageURL
+//        self.address = address
+//        self.distance = distance
+//    }
+
+    private let json: JSON
+    
+    init(json: JSON) {
+        self.json = json
+    }
+    
+    
+    
 }
