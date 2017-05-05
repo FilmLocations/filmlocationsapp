4889efe6cd57688e89e4d2d2e2984f3ed0f5f727
LauraPrichici <p_laura_c@yahoo.com>
2017-05-05 11:14:39 -0700

    Hamburger menu was added.

--- Film Locations/Film Locations/HamburgerMenuController.swift
+++ Film Locations/Film Locations/HamburgerMenuController.swift
@@ -10,26 +10,63 @@ import UIKit
 
 class HamburgerMenuController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var menu = ["Map View", "List View", "Profile", "About"]
+    var viewControllers: [UIViewController] = []
+    var hamburgerViewController: HamburgerViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.rowHeight = 70
+//        tableView.rowHeight = tableView.frame.height / CGFloat(menu.count)
+        
+        let mapStoryBoard = UIStoryboard(name: "Map", bundle: nil)
+        let mapNavigationController = mapStoryBoard.instantiateViewController(withIdentifier: "MapNavigationController")
+        
+        let listStoryBoard = UIStoryboard(name: "List", bundle: nil)
+        let listNavigationController = listStoryBoard.instantiateViewController(withIdentifier: "ListNavigationController")
+
+        let profileStoryBoard = UIStoryboard(name: "Profile", bundle: nil)
+        let profileNavigationController = profileStoryBoard.instantiateViewController(withIdentifier: "ProfileNavigationController")
+        
+        let aboutStoryBoard = UIStoryboard(name: "About", bundle: nil)
+        let aboutNavigationController = aboutStoryBoard.instantiateViewController(withIdentifier: "AboutNavigationController")
+        
+        viewControllers.append(mapNavigationController)
+        viewControllers.append(listNavigationController)
+        viewControllers.append(profileNavigationController)
+        viewControllers.append(aboutNavigationController)
+        
+        // setup the screen displayed after launching
+        hamburgerViewController.contentViewController = mapNavigationController
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension HamburgerMenuController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menu.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath)
+        cell.textLabel?.text = menu[indexPath.row]
+        
+        return cell
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        hamburgerViewController.contentViewController = viewControllers[indexPath.row]
     }
-    */
-
 }
