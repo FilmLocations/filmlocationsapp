e0468504c7434e2d92cf8e0efdbb9fecc3f9d1a5
LauraPrichici <p_laura_c@yahoo.com>
2017-05-07 00:46:20 -0700

    Added filters to List view.

--- Film Locations/Film Locations/ListViewCell.swift
+++ Film Locations/Film Locations/ListViewCell.swift
@@ -15,7 +15,6 @@ class ListViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var yearLabel: UILabel!
     @IBOutlet weak var numberOfLocationsLabel: UILabel!
-    @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var posterImageView: UIImageView!
     
 
@@ -31,7 +30,6 @@ class ListViewCell: UITableViewCell {
         titleLabel.text = nil
         yearLabel.text = nil
         numberOfLocationsLabel.text = nil
-        locationLabel.text = nil
         posterImageView.image = nil
         
         if let movie = movie {
@@ -39,14 +37,15 @@ class ListViewCell: UITableViewCell {
             titleLabel.attributedText = InternalConfiguration.customizeTextAppearance(text: movie.title)
             yearLabel.attributedText = InternalConfiguration.customizeTextAppearance(text: movie.releaseYear)
                         
-            let numberOfLocation = movie.locations.count
-            if numberOfLocation == 1 {
-                locationLabel.attributedText = InternalConfiguration.customizeTextAppearance(text: "LOCATION")
+            var numberOfLocation = "\(movie.locations.count)"
+            if movie.locations.count == 1 {
+                numberOfLocation = numberOfLocation + " LOCATION"
             }
             else {
-                locationLabel.attributedText = InternalConfiguration.customizeTextAppearance(text: "LOCATIONS")
+                numberOfLocation = numberOfLocation + " LOCATIONS"
             }
-            numberOfLocationsLabel.attributedText = InternalConfiguration.customizeTextAppearance(text: "\(numberOfLocation)")
+            
+            numberOfLocationsLabel.attributedText = InternalConfiguration.customizeTextAppearance(text: numberOfLocation)
             
             if let posterImageURL = movie.posterImageURL {
                 posterImageView.setImageWith(posterImageURL)
