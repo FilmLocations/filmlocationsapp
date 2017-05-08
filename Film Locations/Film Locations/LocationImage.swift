779a6104114388ae7245a31fcbdf004ba14b1e9f
Jessica Thrasher <jthrashe@cisco.com>
2017-05-08 10:38:06 -0700

    Adding ability to get all images uploaded by a user

--- Film Locations/Film Locations/LocationImage.swift
+++ Film Locations/Film Locations/LocationImage.swift
@@ -13,11 +13,13 @@ class LocationImage {
     var description: String
     var userId: String
     var timestamp: String
+    var placeId: String
     
-    init(imageURL: String, description: String, userId: String, timestamp: String) {
+    init(imageURL: String, description: String, userId: String, timestamp: String, placeId: String) {
         self.imageURL = imageURL
         self.description = description
         self.userId = userId
         self.timestamp = timestamp
+        self.placeId = placeId
     }
 }
