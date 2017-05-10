f8fddae89a2053e451b16cd7fb93e1e646adcc0a
LauraPrichici <p_laura_c@yahoo.com>
2017-05-09 20:18:28 -0700

    Colors were added

--- Film Locations/Film Locations/InternalConfiguration.swift
+++ Film Locations/Film Locations/InternalConfiguration.swift
@@ -12,6 +12,8 @@ import SwiftyJSON
 struct InternalConfiguration {
     static let mapToggleIcon = "mapToggleIcon"
     static let listToggleIcon = "listToggleIcon"
+    static let navigationBarColor = UIColor(red: 87/255, green: 79/255, blue: 124/255, alpha: 1.0)
+    static let selectedCellColor = UIColor(red: 75/255, green: 64/255, blue: 127/255, alpha: 1)
     
     static func customizeTextAppearance(text: String) -> NSAttributedString {
 //        let shadow = NSShadow()
@@ -23,4 +25,8 @@ struct InternalConfiguration {
         return NSAttributedString(string: text, attributes: [NSForegroundColorAttributeName: attributeColor/*,NSShadowAttributeName: shadow*/])
     }
     
+    static func customizeNavigationBar(navigationController: UINavigationController?) {
+        navigationController?.navigationBar.barTintColor = navigationBarColor
+        navigationController?.navigationBar.tintColor = .white
+    }
 }
