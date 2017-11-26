d8101fbd66631a24160ab6155d682cfb5b83f0ca
Jessica Thrasher <jessica.thrasher@gmail.com>
2017-11-25 16:00:14 -0800

    Updating buddybuild script

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -4,3 +4,8 @@ echo "Copying secure Keys.plist to source directory"
 
 mv ${BUDDYBUILD_SECURE_FILES}/Keys.plist Film\ Locations/Film\ Locations/
 
+echo "Copying xcconfig files"
+
+mv ${BUDDYBUILD_SECURE_FILES}/Debug.xcconfig Film\ Locations/
+mv ${BUDDYBUILD_SECURE_FILES}/Release.xcconfig Film\ Locations/
+
